6e29c8eaa04f5068803c7ab4bbf294820d9deed5
DanielGuzmanT <jhairguz.t@gmail.com>
2018-11-16 22:31:53 -0500

    batalla pokemon avanzada a 90%

--- Batalla_Pokemon/src/Clases/Pokemon.java
+++ Batalla_Pokemon/src/Clases/Pokemon.java
@@ -1,4 +1,86 @@
 package Clases;
 
 public class Pokemon {
+    // ATRIBUTOS
+    private String nombre;
+    private int ataque;
+    private int defensa;
+    private int vida;
+
+    // CONSTRUCTOR
+    public Pokemon(String nombre, int ataque, int defensa, int vida) {
+        this.nombre = nombre;
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.vida = vida;
+    }
+
+    // GETTERS Y SETTERS PARA ACCESO DESDE FUERA DE LA CLASE A LOS ATRIBUTOS PRIVATE
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+
+    public int getDefensa() {
+        return defensa;
+    }
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+
+    // METODOS
+    /*
+    * Este método recibe como parámetro al objeto Pokemon a atacar.
+    * Se diminuye la vida del pokemon pasado como parametro de acuerdo al ataque del pokemon que declara ataque.
+    * Se usa Math.random() para determinar por sorteo si ataque aumenta en un porcentaje o no.
+    * Se usa tambien el metodo defenderse() el pokemon atacado.
+    * */
+    public void atacar(Pokemon pokemon){
+        int ataqueAux = this.ataque;
+        // la suerte del atacante depende de su fuerza de ataque
+        int suerteAtack = (int)(Math.random()*this.ataque)+1;
+        this.ataque += (suerteAtack < 30 ? 10 : 0);
+        // ahora realizamos el ataque, el pokemon intentara defenderse
+        pokemon.defenderse(this);
+        this.ataque = ataqueAux;
+    }
+
+    /*
+     * Ahhhh, me da pereza comentar este metodo
+     */
+    private void defenderse(Pokemon pokemon){
+        // la suerte de la defensa depende de cuanta defensa posee el pokemon
+        int suerteDefense = (int)(Math.random()*this.defensa) + 1;
+        // altas probabilidades de defenderse si posee poca defensa
+        if(suerteDefense<30){
+            System.out.print(this.nombre + " se ha defendido!!");
+            if(this.defensa < pokemon.getAtaque()){
+                System.out.println(" - PERO EL ATAQUE HA SIDO EFECTIVO! :c");
+                this.vida -= pokemon.getAtaque() + this.defensa;
+            } else {
+                System.out.println(" - BUENA DEFENSA!!");
+            }
+        } else{
+            // bajas probabilidades de defenderse si posee bastante defensa
+            System.out.println(this.nombre + " no ha podido defenderse!!");
+        }
+        System.out.println("Ataque de "+pokemon.getNombre()+": "+pokemon.getAtaque());
+        System.out.println("Vida de "+this.nombre+": "+this.vida+"\n\n");
+    }
 }
--- Batalla_Pokemon/src/Main.java
+++ Batalla_Pokemon/src/Main.java
@@ -1,7 +1,17 @@
+import Clases.Pokemon;
+import Clases.Referi;
 
 public class Main {
 
     public static void main(String[] args) {
         // aquí sí podemos pelear, Vegeta
+        Pokemon pikachu = new Pokemon("Pikachu",20,20,400);
+        Pokemon charmander = new Pokemon("Charmander",50,30,800);
+        Referi referi = new Referi();
+
+        // inicia la batalla
+        referi.presentarse(pikachu,charmander);
+        referi.iniciarBatallaPokemon(pikachu,charmander);
+        referi.declararGanador(pikachu,charmander);
     }
 }
